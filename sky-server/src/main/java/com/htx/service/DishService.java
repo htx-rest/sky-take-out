61cc224e9f9c901b89ebe7a192917113b6d3d7e7
鲸落 <1656855152@qq.com>
2024-08-26 23:54:25 +0800

    修改菜品

--- sky-server/src/main/java/com/htx/service/DishService.java
+++ sky-server/src/main/java/com/htx/service/DishService.java
@@ -4,6 +4,7 @@ import com.htx.dto.DishDTO;
 import com.htx.dto.DishPageQueryDTO;
 import com.htx.entity.Dish;
 import com.htx.result.PageResult;
+import com.htx.vo.DishVO;
 
 import java.util.List;
 
@@ -36,4 +37,19 @@ public interface DishService {
      * @param ids
      */
     void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     *
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 根据id修改菜品基本信息和对应的口味信息
+     *
+     * @param dishDTO
+     */
+    void updateWithFlavor(DishDTO dishDTO);
 }
