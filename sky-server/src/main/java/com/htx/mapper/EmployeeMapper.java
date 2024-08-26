6f5d38bc28860cd4d9ed72153fe3c893b4fdc0f3
鲸落 <1656855152@qq.com>
2024-08-26 23:30:13 +0800

    公共字段自动填充

--- sky-server/src/main/java/com/htx/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/htx/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.htx.mapper;
 
 import com.github.pagehelper.Page;
+import com.htx.annotation.AutoFill;
 import com.htx.dto.EmployeePageQueryDTO;
 import com.htx.entity.Employee;
+import com.htx.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -31,6 +33,7 @@ public interface EmployeeMapper {
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
             "values " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
     /**
@@ -44,6 +47,7 @@ public interface EmployeeMapper {
      * 根据主键动态修改属性
      * @param employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
